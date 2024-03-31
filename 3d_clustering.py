3dd5cf9588c5f7e0e3e27913d4703ede4abd28b6
EJ <ejwmercer@gmail.com>
2024-03-30 22:50:27 -0600

    Create figures for tSNE and UMAP

--- 3d_clustering.py
+++ 3d_clustering.py
@@ -48,45 +48,157 @@ def save_projection(X, y, filename, clazz, **kwargs):
     X_new = proj.fit_transform(X, y)
     np.save(f"projections/{filename}", X_new)
     print(f"Saved {filename}.")
-    return X_new
+    return proj
 
 
-if __name__ == "__main__":
+def save_prediction(model, X, y, filename):
+    print(f"Doing predicting for {filename}...")
+    X_new = model.transform(X)
+    np.save(f"projections/{filename}", X_new)
+    print(f"Saved {filename}.")
+
+
+def make_projections():
     # Load in the partial dataset
     X, y = to_xy_numeric(*generate_sample(.1, random_state=19438))
-    print(y.shape)
+    np.save("projections/y_part.npy", y)
     # Do the TSNE projections & save
-    for p in [2, 5, 10, 30, 50, 100, 200]:
-        save_projection(X, y, f"tsne_perp{p}.npy", TSNE, n_components=2, perplexity=p, n_jobs=-1)
+    # for p in [2, 5, 10, 30, 50, 100, 200]:
+    #     save_projection(X, y, f"tsne_perp{p}.npy", TSNE, n_components=2, perplexity=p, n_jobs=-1)
     # Do the UMAP unsupervised projections & save
-    save_projection(X, None, "umap_unsup_2d.npy", UMAP, n_epochs=500, n_components=2, n_jobs=-1)
-    save_projection(X, None, "umap_unsup_3d.npy", UMAP, n_epochs=500, n_components=3, n_jobs=-1)
+    save_projection(X, None, "umap_unsup_2d.npy", UMAP, n_epochs=500, n_components=2, n_jobs=-1, n_neighbors=50)
+    save_projection(X, None, "umap_unsup_3d.npy", UMAP, n_epochs=500, n_components=3, n_jobs=-1, n_neighbors=50)
     # Do supervised UMAP projections & save
-    save_projection(X, y, "umap_sup_2d_def.npy", UMAP, n_epochs=500, n_components=2, n_jobs=-1)
-    save_projection(X, y, "umap_sup_3d_def.npy", UMAP, n_epochs=500, n_components=3, n_jobs=-1)
-    save_projection(X, y, "umap_sup_2d_grid.npy", UMAP, n_epochs=500, n_components=2, min_dist=1., n_neighbors=9, learning_rate=140.,
+    save_projection(X, y, "umap_sup_2d_def.npy", UMAP, n_epochs=500, n_components=2, n_jobs=-1, n_neighbors=50)
+    save_projection(X, y, "umap_sup_3d_def.npy", UMAP, n_epochs=500, n_components=3, n_jobs=-1, n_neighbors=50)
+    save_projection(X, y, "umap_sup_2d_grid.npy", UMAP, n_epochs=500, n_components=2, min_dist=1., n_neighbors=9,
+                    learning_rate=140.,
                     n_jobs=-1)
-    save_projection(X, y, "umap_sup_3d_grid.npy", UMAP, n_epochs=500, n_components=3, min_dist=1., n_neighbors=9, learning_rate=140.,
+    save_projection(X, y, "umap_sup_3d_grid.npy", UMAP, n_epochs=500, n_components=3, min_dist=1., n_neighbors=9,
+                    learning_rate=140.,
                     n_jobs=-1)
-    save_projection(X, y, "umap_sup_4d_grid.npy", UMAP, n_epochs=500, n_components=4, min_dist=1., n_neighbors=9, learning_rate=140.,
+    save_projection(X, y, "umap_sup_4d_grid.npy", UMAP, n_epochs=500, n_components=4, min_dist=1., n_neighbors=9,
+                    learning_rate=140.,
                     n_jobs=-1)
 
     # Load in the full dataset
     X, y = to_xy_numeric(*generate_sample(1., random_state=193))
+    np.save("projections/y_full.npy", y)
     # Do the supervised UMAP projections for the full set
-    save_projection(X, y, "umap_sup_2d_def_full.npy", UMAP, n_epochs=500, n_components=2, n_jobs=-1)
-    save_projection(X, y, "umap_sup_3d_def_full.npy", UMAP, n_epochs=500, n_components=3, n_jobs=-1)
-    save_projection(X, y, "umap_sup_2d_grid_full.npy", UMAP, n_epochs=500, n_components=2, min_dist=1., n_neighbors=9, learning_rate=140.,
+    sup_2d_def = save_projection(X, y, "umap_sup_2d_def_full.npy", UMAP, n_epochs=500, n_components=2, n_jobs=-1, n_neighbors=50)
+    sup_3d_def = save_projection(X, y, "umap_sup_3d_def_full.npy", UMAP, n_epochs=500, n_components=3, n_jobs=-1, n_neighbors=50)
+    sup_2d_grid = save_projection(X, y, "umap_sup_2d_grid_full.npy", UMAP, n_epochs=500, n_components=2, min_dist=1., n_neighbors=9,
+                    learning_rate=140.,
                     n_jobs=-1)
-    save_projection(X, y, "umap_sup_3d_grid_full.npy", UMAP, n_epochs=500, n_components=3, min_dist=1., n_neighbors=9, learning_rate=140.,
+    sup_3d_grid = save_projection(X, y, "umap_sup_3d_grid_full.npy", UMAP, n_epochs=500, n_components=3, min_dist=1., n_neighbors=9,
+                    learning_rate=140.,
                     n_jobs=-1)
 
     # Load in a portion of the test data
     X, y = to_xy_numeric(*generate_sample(.1, set_name="test_patients", random_state=18478))
+    np.save("projections/y_test.npy", y)
     # Do the supervised UMAP projections for the test set
-    save_projection(X, y, "umap_sup_2d_def_test.npy", UMAP, n_epochs=500, n_components=2, n_jobs=-1)
-    save_projection(X, y, "umap_sup_3d_def_test.npy", UMAP, n_epochs=500, n_components=3, n_jobs=-1)
-    save_projection(X, y, "umap_sup_2d_grid_test.npy", UMAP, n_epochs=500, n_components=2, min_dist=1., n_neighbors=9, learning_rate=140.,
-                    n_jobs=-1)
-    save_projection(X, y, "umap_sup_3d_grid_test.npy", UMAP, n_epochs=500, n_components=3, min_dist=1., n_neighbors=9, learning_rate=140.,
-                    n_jobs=-1)
+    save_prediction(sup_2d_def, X, y, "umap_sup_2d_def_test.npy")
+    save_prediction(sup_3d_def, X, y, "umap_sup_3d_def_test.npy")
+    save_prediction(sup_2d_grid, X, y, "umap_sup_2d_grid_test.npy")
+    save_prediction(sup_3d_grid, X, y, "umap_sup_3d_grid_test.npy")
+
+
+def make_2d_group_plot(files_in, y_file_in, file_out, suptitle, titles, dimensions, shape):
+    # Read in the data
+    X = [np.load(f"projections/{file_in}.npy") for file_in in files_in]
+    y = np.load(f"projections/{y_file_in}.npy")
+
+    # Make each of the subplots
+    plt.figure(figsize=dimensions)
+    for i, (x, title) in enumerate(zip(X, titles)):
+        plt.subplot(*shape, i + 1)
+        plt.title(title)
+        plt.scatter(*x.T, c=y, cmap="rainbow", marker=".")
+    plt.suptitle(suptitle)
+    plt.tight_layout()
+    plt.savefig(f"figures/proj-{file_out}.pdf")
+    plt.show()
+
+
+def make_2d_plot(file_in, y_file_in, title, dimensions):
+    # Read in the data
+    X = np.load(f"projections/{file_in}.npy")
+    y = np.load(f"projections/{y_file_in}.npy")
+
+    # Make the scatter plot and save to the file
+    plt.figure(figsize=dimensions)
+    plt.title(title)
+    plt.scatter(*X.T, c=y, cmap="rainbow", marker=".")
+    plt.tight_layout()
+    plt.savefig(f"figures/proj-{file_in}.pdf")
+    plt.show()
+
+
+def make_3d_plot(file_in, y_file_in, title, dimensions):
+    # Read in the data
+    X = np.load(f"projections/{file_in}.npy")
+    y = np.load(f"projections/{y_file_in}.npy")
+
+    # Make the scatter plot and save to the file
+    fig = plt.figure(figsize=dimensions)
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(*X.T, c=y, cmap="rainbow", marker=".")
+    plt.title(title)
+    plt.tight_layout()
+    plt.savefig(f"figures/proj-{file_in}.pdf")
+    plt.show()
+
+
+def make_4d_plot(file_in, y_file_in, title, dimensions):
+    # Read in the data
+    X = np.load(f"projections/{file_in}.npy")
+    y = np.load(f"projections/{y_file_in}.npy")
+
+    # Make the scatter plot and save to the file
+    plt.figure(figsize=dimensions)
+    plt.subplot(131)
+    plt.title("Comp 1 v. Comp 2")
+    plt.scatter(*X.T[[0, 1]], c=y, cmap="rainbow", marker=".")
+    plt.subplot(132)
+    plt.title("Comp 1 v. Comp 3")
+    plt.scatter(*X.T[[0, 2]], c=y, cmap="rainbow", marker=".")
+    plt.subplot(133)
+    plt.title("Comp 1 v. Comp 4")
+    plt.scatter(*X.T[[0, 3]], c=y, cmap="rainbow", marker=".")
+    plt.suptitle(title)
+    plt.tight_layout()
+    plt.savefig(f"figures/proj-{file_in}.pdf")
+    plt.show()
+
+
+if __name__ == "__main__":
+    SIZE_FACTOR = 3
+    #make_projections()
+
+    # Make the TSNE plot
+    perps = [2, 5, 10, 30, 50, 100]
+    make_2d_group_plot([f"tsne_perp{p}" for p in perps], "y_part", "tsne",
+                       ''"TSNE Trained on 10% of the Test Data", [f"Perplexity: {p}" for p in perps], (SIZE_FACTOR * 3, SIZE_FACTOR * 2), (2, 3))
+
+    # Make the 2D UMAP figures
+    make_2d_plot("umap_unsup_2d", "y_part", "Unsupervised UMAP in 2D\n(10% of training data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_2d_plot("umap_sup_2d_def", "y_part", "Supervised UMAP in 2D\n(10% of training data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_2d_plot("umap_sup_2d_grid", "y_part", "Supervised UMAP in 2D\n(10% of training data, grid-searched params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_2d_plot("umap_sup_2d_def_full", "y_full", "Supervised UMAP in 2D\n(Training data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_2d_plot("umap_sup_2d_grid_full", "y_full", "Supervised UMAP in 2D\n(Training data, grid-searched params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_2d_plot("umap_sup_2d_def_test", "y_test", "Supervised UMAP in 2D\n(Test data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_2d_plot("umap_sup_2d_grid_test", "y_test", "Supervised UMAP in 2D\n(Test data, grid-searched params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+
+    # Make the 3D UMAP figures
+    make_3d_plot("umap_unsup_3d", "y_part", "Unsupervised UMAP in 3D\n(10% of training data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_3d_plot("umap_sup_3d_def", "y_part", "Supervised UMAP in 3D\n(10% of training data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_3d_plot("umap_sup_3d_grid", "y_part", "Supervised UMAP in 3D\n(10% of training data, grid-searched params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_3d_plot("umap_sup_3d_def_full", "y_full", "Supervised UMAP in 3D\n(Training data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_3d_plot("umap_sup_3d_grid_full", "y_full", "Supervised UMAP in 3D\n(Training data, grid-searched params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_3d_plot("umap_sup_3d_def_test", "y_test", "Supervised UMAP in 3D\n(Test data, 50 neighbors, default params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+    make_3d_plot("umap_sup_3d_grid_test", "y_test", "Supervised UMAP in 3D\n(Test data, grid-searched params)", (SIZE_FACTOR * 2, SIZE_FACTOR * 2))
+
+    # Make the 4D UMAP figure
+    make_4d_plot("umap_sup_4d_grid", "y_part", "Supervised UMAP into 4D\n(using parameters found via GridSearch)",
+                (15, 5))
